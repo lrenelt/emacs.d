1a1426e8c054c240285b800a9a3e331040f3b66d
Lukas Renelt <lukas.renelt@uni-muenster.de>
2025-01-06 10:43:12 +0100

    references in latex

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -18,7 +18,28 @@
 (add-hook 'objc-mode-hook 'irony-mode)
 (add-hook 'irony-mode-hook 'irony-cbd-autosetup-compile-options)
 
+;; use cleveref in LaTeX
+(add-hook 'LaTeX-mode-hook
+          (lambda nil
+            (setq reftex-ref-style-default-list '("Default" "Cleveref"))
+            )
+          )
+
+
 ;;; include DUNE macros
 (require 'init-dune nil t)
+
+;;; load reftex in latex mode
+(add-hook 'LaTeX-mode-hook 'turn-on-reftex)
+(setq reftex-plug-into-AUCTeX t)
+(setq TeX-shell-escape nil)
+(eval-after-load "tex"
+  '(add-to-list 'TeX-expand-list
+                '("%(shellesc)"
+                  (lambda nil
+                    (if
+                        (eq TeX-shell-escape t)
+                        "-shell-escape" ""))
+                  )))
 
 (provide 'init-local)
